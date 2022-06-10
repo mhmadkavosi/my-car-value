f3543e649bcedad4b6f41303c5ed42c40d42905c
mhmad.kavosi <mhmad.kavosi@gmail.com>
2022-06-10 18:12:22 +0430

    add not found exception

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { Repository } from 'typeorm';
 import { InjectRepository } from '@nestjs/typeorm';
 import { User } from './users.entity';
@@ -25,7 +25,7 @@ export class UsersService {
     const user = await this.findOne(id);
 
     if (!user) {
-      throw new Error('user not found !');
+      throw new NotFoundException('user not found !');
     }
 
     Object.assign(user, attrs);
@@ -35,8 +35,8 @@ export class UsersService {
   async remove(id: number) {
     const user = await this.findOne(id);
     if (!user) {
-      throw new Error('user not found !');
+      throw new NotFoundException('user not found !');
     }
-    return this.repo.save(user);
+    return this.repo.remove(user);
   }
 }
