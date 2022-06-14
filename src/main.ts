ef61f5adb7a84a54441532a76a26f5519a0d1f8a
mhmad.kavosi <mhmad.kavosi@gmail.com>
2022-06-14 17:54:58 +0430

    init cookie session

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,15 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+const session = require('cookie-session');
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.use(
+    new session({
+      keys: ['ldskajfafdl'],
+    }),
+  );
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
