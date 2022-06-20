5bd5be5dbf23fdc5a8a80725336f136527a927b9
mhmad.kavosi <mhmad.kavosi@gmail.com>
2022-06-20 21:36:19 +0430

    add e2e test for auth service

--- src/main.ts
+++ src/main.ts
@@ -1,20 +1,9 @@
-import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-const session = require('cookie-session');
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.use(
-    new session({
-      keys: ['ldskajfafdl'],
-    }),
-  );
-  app.useGlobalPipes(
-    new ValidationPipe({
-      whitelist: true,
-    }),
-  );
+
   await app.listen(3000);
 }
 bootstrap();
