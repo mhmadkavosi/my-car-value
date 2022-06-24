6a83aafea6f8c3451d382b6c83156e3690a3b975
mhmad.kavosi <mhmad.kavosi@gmail.com>
2022-06-24 21:37:58 +0430

    use service to create report

--- src/reports/reports.controller.ts
+++ src/reports/reports.controller.ts
@@ -1,4 +1,15 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post, UseGuards } from '@nestjs/common';
+import { ReportsService } from './reports.service';
+import { AuthGurd } from '../gurds/auth.gurds';
+import { CreateReportDto } from './dtos/create-report.dto';
 
 @Controller('reports')
-export class ReportsController {}
+export class ReportsController {
+  constructor(private reportsService: ReportsService) {}
+
+  @Post()
+  @UseGuards(AuthGurd)
+  createReport(@Body() body: CreateReportDto) {
+    return this.reportsService.create(body);
+  }
+}
